e40318070e0e8d90dba9fbf6321975ea327b1c82
Kyle Sweigart <kasweigart@gmail.com>
2020-06-04 17:14:29 -0400

    set up react and express

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import RSNavbar from './components/NavbarToggler'
+
+function App() {
+  return (<div>
+    <RSNavbar />
+  </div>
+  );
+}
+
+export default App;
