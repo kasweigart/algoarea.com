79154c2125bdea4cc0486f842134fb1417396162
Kyle Sweigart <kasweigart@gmail.com>
2020-06-10 18:21:00 -0400

    finished home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -17,10 +17,12 @@ const Home = (props) => {
         <img src={home} alt="" className='mt-4 mb-4 img-fluid'/>
         <p>Below you will find what this platform has to offer.</p>
         <p className="lead">
+        <a href='#tour'>
           <Button color="info">Take the Tour</Button>
+          </a>
         </p>
       </Jumbotron>
-      <Jumbotron className='mb-0 bg-white'>
+      <Jumbotron className='mb-0 bg-white' id='tour'>
         <h1 className="display-4">Algorithms</h1>
         <p className="lead font-italic">A process or set of rules to be followed in calculations or other problem-solving operations, especially by a computer.<br></br>google.com</p>
         <hr className="my-2" />
@@ -37,7 +39,7 @@ const Home = (props) => {
         <p className="lead font-italic">A data structure is a data organization, management, and storage format that enables efficient access and modification.<br></br>wikipedia.org</p>
         <hr className="my-2" />
         <img src={ds} alt="" className='img-fluid w-50 mt-1 mb-2'/>
-        <p>Below you will find what this platform has to offer.</p>
+        <p>Learn about data structures in a system and explore how they are used to perform a task.</p>
         <p className="lead">
         <Link to='/data-structures'>
           <Button color="info">Go to Data Structures</Button>
@@ -49,7 +51,7 @@ const Home = (props) => {
         <p className="lead font-italic">Time complexity of an algorithm quantifies the amount of time taken by an algorithm to run as a function of the length of the input. Similarly, Space complexity of an algorithm quantifies the amount of space or memory taken by an algorithm to run as a function of the length of the input.<br></br>hackerearth.com</p>
         <hr className="my-2" />
         <img src={tsc} alt="" className='img-fluid w-50 mt-2 mb-2'/>
-        <p>Below you will find what this platform has to offer.</p>
+        <p>Analyze the relation of space and time to understand why certain methods perform better than others.</p>
         <p className="lead">
           <Button color="info">Go to Time & Space Complexity</Button>
         </p>
@@ -59,7 +61,6 @@ const Home = (props) => {
         <p className="lead">Various algorithmic exercises to solidify your working knowledge of algorithms and data structures.</p>
         <hr className="my-2" />
         <img src={exercise} alt="" className='img-fluid mb-2'/>
-        <p>Below you will find what this platform has to offer.</p>
         <p className="lead">
           <Button color="info">Go to Exercises</Button>
         </p>
@@ -68,7 +69,7 @@ const Home = (props) => {
         <h1 className="display-4">Resources</h1>
         <p className="lead">A section of helpful resources to guide you through your journey of complex world of computer science.</p>
         <hr className="my-2" />
-        <p>Below you will find what this platform has to offer.</p>
+        <p></p>
         <p className="lead">
           <Button color="info">Go to Resources</Button>
         </p>
