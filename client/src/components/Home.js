843c670ad5d7c103b960ae789357a1dc5e64c2aa
Kyle Sweigart <kasweigart@gmail.com>
2020-06-10 18:31:36 -0400

    home final

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -24,7 +24,7 @@ const Home = (props) => {
       </Jumbotron>
       <Jumbotron className='mb-0 bg-white' id='tour'>
         <h1 className="display-4">Algorithms</h1>
-        <p className="lead font-italic">A process or set of rules to be followed in calculations or other problem-solving operations, especially by a computer.<br></br>google.com</p>
+        <p className="lead font-italic">A process or set of rules to be followed in calculations or other problem-solving operations, especially by a computer.<br></br>(google.com)</p>
         <hr className="my-2" />
         <img src={algorithm} alt="" className='img-fluid mt-3 mb-3'/>
         <p>Study how and why the steps of different alogithms output their expected results.</p>
@@ -36,7 +36,7 @@ const Home = (props) => {
       </Jumbotron>
       <Jumbotron  className='mb-0 bg-white'>
         <h1 className="display-4">Data Structures</h1>
-        <p className="lead font-italic">A data structure is a data organization, management, and storage format that enables efficient access and modification.<br></br>wikipedia.org</p>
+        <p className="lead font-italic">A data structure is a data organization, management, and storage format that enables efficient access and modification.<br></br>(wikipedia.org)</p>
         <hr className="my-2" />
         <img src={ds} alt="" className='img-fluid w-50 mt-1 mb-2'/>
         <p>Learn about data structures in a system and explore how they are used to perform a task.</p>
@@ -48,9 +48,9 @@ const Home = (props) => {
       </Jumbotron>
       <Jumbotron className='mb-0 bg-white'>
         <h1 className="display-4">Time & Space Complexity</h1>
-        <p className="lead font-italic">Time complexity of an algorithm quantifies the amount of time taken by an algorithm to run as a function of the length of the input. Similarly, Space complexity of an algorithm quantifies the amount of space or memory taken by an algorithm to run as a function of the length of the input.<br></br>hackerearth.com</p>
+        <p className="lead font-italic">Time complexity of an algorithm quantifies the amount of time taken by an algorithm to run as a function of the length of the input. Similarly, Space complexity of an algorithm quantifies the amount of space or memory taken by an algorithm to run as a function of the length of the input.<br></br>(hackerearth.com)</p>
         <hr className="my-2" />
-        <img src={tsc} alt="" className='img-fluid w-50 mt-2 mb-2'/>
+        <img src={tsc} alt="" className='img-fluid w-75 mt-2 mb-2'/>
         <p>Analyze the relation of space and time to understand why certain methods perform better than others.</p>
         <p className="lead">
           <Button color="info">Go to Time & Space Complexity</Button>
@@ -67,7 +67,7 @@ const Home = (props) => {
       </Jumbotron>
       <Jumbotron className='mb-0' style={{backgroundColor: '#f7f9fa'}}>
         <h1 className="display-4">Resources</h1>
-        <p className="lead">A section of helpful resources to guide you through your journey of complex world of computer science.</p>
+        <p className="lead">A section of helpful resources to guide you through your journey of the complex world of computer science.</p>
         <hr className="my-2" />
         <p></p>
         <p className="lead">
